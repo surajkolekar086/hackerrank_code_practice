89b8c9aa7f0ddffd6509d53bbad9b7d92c800371
Suraj Jalindar Kolekar(UST <242840@ust.com>
2025-05-05 16:38:48 +0530

    default argument python example

---
+++ Easy/default_argument.py
@@ -0,0 +1,58 @@
+"""
+Problem: https://www.hackerrank.com/challenges/default-arguments/problem?isFullScreen=true
+
+Input:
+3
+odd 2
+even 3
+odd 5
+
+Output:
+1
+3
+0
+2
+4
+1
+3
+5
+7
+9
+
+
+"""
+
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+#Change code in  print_from_stream() method only...      
+def print_from_stream(n, stream=None):
+    if stream is None:
+        stream = EvenStream()
+    for _ in range(n):
+        print(stream.get_next())
+
+
+queries = int(input())
+for _ in range(queries):
+    stream_name, n = input().split()
+    n = int(n)
+    if stream_name == "even":
+        print_from_stream(n)
+    else:
+        print_from_stream(n, OddStream())
