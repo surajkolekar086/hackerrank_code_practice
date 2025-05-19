28ea63f3b65d49d46d0101f4c2627924ed3a4f93
Suraj Jalindar Kolekar(UST <242840@ust.com>
2025-05-19 12:06:54 +0530

    string split and join example comment added

--- Easy/strings/split_and_join_string.py
+++ Easy/strings/split_and_join_string.py
@@ -1,3 +1,10 @@
+"""
+problem: https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+input: this is a string
+ouput: this-is-a-string
+"""
+
 def split_and_join(line):
     # write your code here
     a = line.split(" ")
@@ -7,3 +14,10 @@ if __name__ == '__main__':
     line = input()
     result = split_and_join(line)
     print(result)
+
+    """
+Method	Purpose	Input Type	Output Type
+    split()	Split string → list	str	list
+    join()	Join list → string (with separator)	iterable of str	str
+    
+    """
